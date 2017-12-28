8262b03d08676c24a687716a0ecf68f684aa6360
cs-matheus-galdino <matheus.galdino@hotmail.com>
2017-12-28 09:50:48 -0200

    Added payment routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import paymentRoutes from './payment';
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.status(404).json({ message: 'Endereço não encontrado' }));
+
+router.use('/payment', paymentRoutes);
 
 export default router;
