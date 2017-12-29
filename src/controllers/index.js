25350f56e515d21eb06a060de79de05610696420
Antonio Fraideinberze <antonio.fraideinberze@concrete.com.br>
2017-12-29 08:48:27 -0200

    Logger service connected.

---
+++ src/controllers/index.js
@@ -0,0 +1,30 @@
+import Payment from '../models';
+
+export const PaymentsController = PaymentObject => ({
+  async processPayment(req, res) {
+    try {
+      const payment = new PaymentObject(req.body);
+      await payment.save();
+      res.status(200).send(payment);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+
+    return res.json('processPayment successfuly reached');
+  },
+
+  async checkPaymentStatus(req, res) {
+    try {
+      const payment = await PaymentObject.find({ _id: req.param.id });
+      res.status(200).send(payment);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  },
+
+  notFound(req, res) {
+    res.sendStatus(404);
+  },
+});
+
+export default PaymentsController(Payment);
